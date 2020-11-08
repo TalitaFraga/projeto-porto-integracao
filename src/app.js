f7a27d1ce9a81df31da7bb2731d33e484828f4e6
TalitaFraga <talitad.fraga@gmail.com>
2020-11-07 21:08:05 -0300

    api completa

--- src/app.js
+++ src/app.js
@@ -1,7 +1,18 @@
 const express = require("express")
 const bodyParser = require("body-parser")
+const mongoose = require("mongoose")
 
 const app = express()
+
+mongoose.connect("mongodb://localhost:27017/clientes", { useNewUrlParser: true, useUnifiedTopology: true })
+
+let db = mongoose.connection 
+
+db.on("error", console.log.bind(console, "connection error:"))
+db.once("open", function (){
+  console.log("conexão feita com sucesso")
+})
+
 
 //rotas
 const clientes = require("./routes/clientesRoute")
--- src/controllers/clientesController.js
+++ src/controllers/clientesController.js
@@ -1,22 +1,43 @@
+const clientes = require('../models/clientes')
+
 const getAll = (req, res) => {
-    console.log("getAll");
-      res.status(200).send("ok");
+    clientes.find((err, clientes) => {
+      if (err) {
+        res.status(500).send({message: err.message})
+      }
+      res.status(200).send(clientes)
+    })
 };
 
 const getCompradores = (req, res) => {
-    console.log("getCompradores");
-      res.status(200).send("ok");
+    clientes.find({comprou:true},(err, clientes) => {
+      if (err) {
+        res.status(500).send({message: err.message})
+      }
+      res.status(200).send(clientes.map(({ nome, email }) => ({nome, email})));
+    })
+
 };
 
 const getByCpf = (req, res) => {
-    console.log("getByCpf");
-      res.status(200).send("ok");
+    clientes.find({ cpf: req.params.cpf },(err, clientes) => {
+      if (err) {
+        res.status(500).send({message: err.message})
+      }
+      res.status(200).send(clientes);
+    })
 };
 
 const postCliente = (req, res) => {
-    console.log("postCliente");
-      res.status(200).send("ok");
-};
+  const cliente = new clientes(req.body)
+
+  cliente.save(err  => {
+    if (err) {
+      res.status(500).send({ message: err.message})
+    }
+    res.status(201).send(cliente.toJSON())
+  })
+}
 
 module.exports = {
     getAll,
